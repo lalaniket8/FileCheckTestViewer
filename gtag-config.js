1f6765bacc885cfcc9b8a3a4dd8c141bfb397d9c
anikelal <anikelal@amd.com>
2026-04-10 15:39:44 +0530

    gtag fix

---
+++ gtag-config.js
@@ -0,0 +1,7 @@
+/* Google tag (gtag.js) bootstrap — external file so CSP need not allow 'unsafe-inline'. */
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "G-07BKGSLNRC");
